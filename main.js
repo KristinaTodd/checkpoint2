d901caa3f4828822a83d2330585a2f4d9c2ff384
Kristina Todd <Kristina.Todd22@gmail.com>
2020-02-02 11:59:20 -0700

    functionality cleanup

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 let currentGoldCount = 0
 let goldCountElem = document.getElementById("gold-count")
 
+let autoItems = {
+  maiden: { total: 0, cost: 125, modifier: 3 },
+  ship: { total: 0, cost: 750, modifier: 10 },
+  canon: { total: 0, cost: 25, modifier: 1 }
+}
 
-let maiden = { total: 0, cost: 125, modifier: 3 }
-let canon = { total: 0, cost: 25, modifier: 1 }
+
 let parrot = { total: 0, cost: 200, modifier: 1 }
-let ship = { total: 0, cost: 750, modifier: 10 }
+
 let hat = { total: 0, cost: 1200, modifier: 0 }
 
 function pillageIsland() {
@@ -28,15 +32,65 @@ function updateGoldCount() {
   canonReady()
   maidenReady()
   shipReady()
-  maidenNotReady()
-  canonNotReady()
-  shipNotReady()
-  parrotNotReady()
+
 }
 
 //TODO consolidate all single item buy functions into one function that takes in the item being bought
 //TODO consolidate hat & parrot into one function
+function buyAuto(autoItem) {
+  currentGoldCount = currentGoldCount - autoItems[autoItem].cost
+  updateGoldCount()
+  saveGame()
 
+  if (autoItem == 'canon') {
+    setInterval(canonFire, 3000)
+    autoItems.canon.total++
+    updateCanonCount()
+    updateCanonMod()
+    autoItems.canon.cost = autoItems.canon.cost + 5
+    updateCanonCost()
+
+    Swal.fire({
+      position: 'top-end',
+      title: 'You purchased a canon!',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  } else if (autoItem == 'maiden') {
+    setInterval(maidenFire, 2000)
+
+    autoItems.maiden.total++
+    updateMaidenCount()
+    updateMaidenMod()
+
+
+    autoItems.maiden.cost = autoItems.maiden.cost + 15
+    updateMaidenCost()
+
+    Swal.fire({
+      position: 'top-end',
+      title: 'You purchased a Pirate Maiden!',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  } else if (autoItem == 'ship') {
+    setInterval(shipFire, 1000)
+
+    autoItems.ship.total++
+    updateShipCount()
+    updateShipMod()
+
+    autoItems.ship.cost = autoItems.ship.cost + 150
+    updateShipCost()
+
+    Swal.fire({
+      position: 'top-end',
+      title: 'You purchased a Pirate Ship!',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+}
 
 //PARROT
 
@@ -62,14 +116,12 @@ function buyParrot() {
 function parrotReady() {
   if (currentGoldCount >= parrot.cost && parrot.total == 0) {
     document.getElementById("buy-parrot").classList.remove("invisible")
-  }
-}
-
-function parrotNotReady() {
-  if (currentGoldCount < parrot.cost) {
+  } else if (currentGoldCount < parrot.cost) {
     document.getElementById("buy-parrot").classList.add("invisible")
   }
 }
+
+
 
 //PIRATE HAT
 
@@ -95,44 +147,21 @@ function buyHat() {
 function hatReady() {
   if (currentGoldCount >= hat.cost && hat.total == 0) {
     document.getElementById("buy-hat").classList.remove("invisible")
-  }
-}
-
-function hatNotReady() {
-  if (currentGoldCount < hat.cost) {
+  } else if (currentGoldCount < hat.cost) {
     document.getElementById("buy-hat").classList.add("invisible")
   }
 }
+
 //CANON 
 
 function canonReady() {
-  if (currentGoldCount >= canon.cost) {
+  if (currentGoldCount >= autoItems.canon.cost) {
     document.getElementById("buy-canon").classList.remove("invisible")
+  } else if (currentGoldCount < autoItems.canon.cost) {
+    document.getElementById("buy-canon").classList.add("invisible")
   }
 }
 
-function buyCanon() {
-  setInterval(canonFire, 3000)
-
-  currentGoldCount = currentGoldCount - canon.cost
-
-  canon.total++
-  updateCanonCount()
-  updateCanonMod()
-
-
-  updateGoldCount()
-
-  canon.cost = canon.cost + 5
-  updateCanonCost()
-
-  Swal.fire({
-    position: 'top-end',
-    title: 'You purchased a canon!',
-    showConfirmButton: false,
-    timer: 1500
-  })
-}
 
 function canonFire() {
   currentGoldCount++
@@ -140,18 +169,13 @@ function canonFire() {
 }
 
 function updateCanonCount() {
-  let canonTemplate = `${canon.total}`
+  let canonTemplate = `${autoItems.canon.total}`
   document.getElementById("canon-total").innerHTML = canonTemplate
 }
 
-function canonNotReady() {
-  if (currentGoldCount < canon.cost) {
-    document.getElementById("buy-canon").classList.add("invisible")
-  }
-}
 
 function updateCanonMod() {
-  let canonMod = canon.total * 0.33
+  let canonMod = autoItems.canon.total * 0.33
   let currentCanonMod = canonMod.toFixed(2)
 
   let template =
@@ -162,39 +186,18 @@ function updateCanonMod() {
 
 function updateCanonCost() {
   let template =
-    `Cost: ${canon.cost}<img src="./gold-coin.png" class="shoppe-gold">`
+    `Cost: ${autoItems.canon.cost}<img src="./gold-coin.png" class="shoppe-gold">`
 
   document.getElementById("canon-cost").innerHTML = template
 }
 //MAIDEN
 
 function maidenReady() {
-  if (currentGoldCount >= maiden.cost) {
+  if (currentGoldCount >= autoItems.maiden.cost) {
     document.getElementById("buy-maiden").classList.remove("invisible")
+  } else if (currentGoldCount < autoItems.maiden.cost) {
+    document.getElementById("buy-maiden").classList.add("invisible")
   }
-}
-
-function buyMaiden() {
-  setInterval(maidenFire, 2000)
-
-  currentGoldCount = currentGoldCount - maiden.cost
-
-  maiden.total++
-  updateMaidenCount()
-  updateMaidenMod()
-
-
-  updateGoldCount()
-
-  maiden.cost = maiden.cost + 15
-  updateMaidenCost()
-
-  Swal.fire({
-    position: 'top-end',
-    title: 'You purchased a Pirate Maiden!',
-    showConfirmButton: false,
-    timer: 1500
-  })
 }
 
 function maidenFire() {
@@ -203,18 +206,12 @@ function maidenFire() {
 }
 
 function updateMaidenCount() {
-  let maidenTemplate = `${maiden.total}`
+  let maidenTemplate = `${autoItems.maiden.total}`
   document.getElementById("maiden-total").innerHTML = maidenTemplate
 }
 
-function maidenNotReady() {
-  if (currentGoldCount < maiden.cost) {
-    document.getElementById("buy-maiden").classList.add("invisible")
-  }
-}
-
 function updateMaidenMod() {
-  let maidenMod = maiden.total * 1.5
+  let maidenMod = autoItems.maiden.total * 1.5
   let currentMaidenMod = maidenMod.toFixed(1)
 
   let maidenTemplate = `<span class="text-center">Generating ${currentMaidenMod} GPS</span>`
@@ -224,38 +221,18 @@ function updateMaidenMod() {
 
 function updateMaidenCost() {
   let template =
-    `Cost: ${maiden.cost}<img src="./gold-coin.png" class="shoppe-gold">`
+    `Cost: ${autoItems.maiden.cost}<img src="./gold-coin.png" class="shoppe-gold">`
 
   document.getElementById("maiden-cost").innerHTML = template
 }
 
 //PIRATE SHIP 
 function shipReady() {
-  if (currentGoldCount >= ship.cost) {
+  if (currentGoldCount >= autoItems.ship.cost) {
     document.getElementById("buy-ship").classList.remove("invisible")
+  } else if (currentGoldCount < autoItems.ship.cost) {
+    document.getElementById("buy-ship").classList.add("invisible")
   }
-}
-
-function buyShip() {
-  setInterval(shipFire, 1000)
-
-  currentGoldCount = currentGoldCount - ship.cost
-  updateGoldCount()
-
-  ship.total++
-  updateShipCount()
-  updateShipMod()
-
-
-  ship.cost = ship.cost + 150
-  updateShipCost()
-
-  Swal.fire({
-    position: 'top-end',
-    title: 'You purchased a Pirate Ship!',
-    showConfirmButton: false,
-    timer: 1500
-  })
 }
 
 function shipFire() {
@@ -264,18 +241,13 @@ function shipFire() {
 }
 
 function updateShipCount() {
-  let shipTemplate = `${ship.total}`
+  let shipTemplate = `${autoItems.ship.total}`
   document.getElementById("ship-total").innerHTML = shipTemplate
 }
 
-function shipNotReady() {
-  if (currentGoldCount < ship.cost) {
-    document.getElementById("buy-ship").classList.add("invisible")
-  }
-}
 
 function updateShipMod() {
-  let currentShipMod = ship.total * 10
+  let currentShipMod = autoItems.ship.total * 10
 
   let shipTemplate =
     `<span class="text-center">Generating ${currentShipMod} GPS</span>`
@@ -285,15 +257,18 @@ function updateShipMod() {
 
 function updateShipCost() {
   let template =
-    `Cost: ${ship.cost}<img src="./gold-coin.png" class="shoppe-gold">`
+    `Cost: ${autoItems.ship.cost}<img src="./gold-coin.png" class="shoppe-gold">`
 
   document.getElementById("ship-cost").innerHTML = template
 }
 
 //TODO set all current data to save locally
 
-// function savePlayers() {
-//   window.localStorage.setItem("players", JSON.stringify(players))
-// }
+function saveGame() {
+  window.localStorage.setItem("game-stats", JSON.stringify(autoItems))
+  window.localStorage.setItem("gold", JSON.stringify(currentGoldCount))
+  window.localStorage.setItem("parrot", JSON.stringify(parrot))
+  window.localStorage.setItem("hat", JSON.stringify(hat))
+}
 
 
